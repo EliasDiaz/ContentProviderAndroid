f73a8cd72e878a93d91c2ca63c8698d0a364a69b
Iphone_RailsDeveloper <fdm1284@hotmail.com>
2011-10-19 17:21:12 -0500

    mod3

--- src/com/fermin/ContProv/Main.java
+++ src/com/fermin/ContProv/Main.java
@@ -1,7 +1,9 @@
 package com.fermin.ContProv;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 
 public class Main extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,10 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+       
+        Cursor people = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        while (people.moveToNext()) {
+			
+		}
     }
 }
